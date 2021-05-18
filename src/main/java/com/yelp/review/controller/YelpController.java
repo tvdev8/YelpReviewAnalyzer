dc7f62e21dc7cd23fc00f1618353b283e7f78282
Proyekto.Ni.Dyet <33678216+tvdev8@users.noreply.github.com>
2021-05-18 09:00:16 +0800

    Update YelpController.java

--- src/main/java/com/yelp/review/controller/YelpController.java
+++ src/main/java/com/yelp/review/controller/YelpController.java
@@ -1,6 +1,5 @@
 package com.yelp.review.controller;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.yelp.review.ParamProperties;
 import com.yelp.review.model.ReviewWrapper;
 import com.yelp.review.model.Reviews;
@@ -28,8 +26,8 @@ public class YelpController {
 	private RestTemplate restTemplate;
 
 	@ResponseBody
-	@GetMapping("/test")
-	public List<Reviews> test(@RequestParam("bussId") String bussId) throws JsonParseException, IOException {
+	@GetMapping("/reviews")
+	public List<Reviews> test(@RequestParam("bussId") String bussId) {
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setBearerAuth(ParamProperties.API_KEY);
